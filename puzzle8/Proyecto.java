4c1deff419f02a221b83098414ea92e066a8cb1d
Angel Gabriel Acuña Silva <l23170060@culiacan.tecnm.mx>
2026-09-23 23:20:17 -0700

    8puzzle

---
+++ puzzle8/Proyecto.java
@@ -0,0 +1,53 @@
+package puzzle8;
+/**
+ *
+ * @author angel
+ */
+import java.util.LinkedList;
+import java.util.List;
+
+public class Proyecto {
+
+    public static void main(String[] args) {
+        Nodo nodo = new Nodo("1238 4765"); 
+        ArbolDeBusqueda ab = new ArbolDeBusqueda(nodo);
+
+ 
+        
+        String objetivo = "1284376 5"; 
+
+
+        Nodo solucion = ab.busquedaPrimeroAnchura(objetivo);
+        
+        if (solucion == null) {
+            System.out.println("no se encontro solucion.");
+            return;
+        }
+
+        System.out.println("estado de solucion: " + solucion.estado);
+        System.out.println("nivel de movimientos " + solucion.getNivel());
+
+        // imprime desde la raiz hasta la solucion
+        List<String> camino = reconstruirCamino(solucion);
+        System.out.println("camino (" + (camino.size() - 1) + " movimientos):");
+        for (String e : camino) {
+            imprimirTablero(e);
+            System.out.println("-----");
+        }
+    }
+
+    static List<String> reconstruirCamino(Nodo n) {
+        LinkedList<String> camino = new LinkedList<>();
+        while (n != null) {
+            camino.addFirst(n.estado);
+            n = n.padre;
+        }
+        return camino;
+    }
+
+    static void imprimirTablero(String e) {
+        System.out.println(e.substring(0, 3));
+        System.out.println(e.substring(3, 6));
+        System.out.println(e.substring(6, 9));
+    }
+}
